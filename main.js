25865fd62726d13c957b0ff9fa6b4f2fbabca2ca
Fabrice <f.nkurunziz@alustudent.com>
2024-07-20 11:41:50 +0200

    return a function inside a function

--- main.js
+++ main.js
@@ -1,14 +1,43 @@
-function outer() {
+
+// ====================================
+// SCOPING
+// ====================================
+
+// function outer() {
+//     let counter = 0;
+
+//     function inner() {
+//         counter  ++;
+//         console.log(counter)
+//     }
+
+//     inner();
+
+//     console.log(counter)
+// }
+
+
+// outer()
+
+
+// ===========================================
+// we can return a function inside a function
+// ===========================================
+
+
+function outerFunction() {
     let counter = 0;
 
-    function inner() {
-        counter  ++;
+    function innerFunction() {
+        counter++;
+        console.log(counter)
     }
 
-    inner();
-
-    console.log(counter)
+    return innerFunction;
 }
 
 
-outer()
+let myFunction = outerFunction();
+
+myFunction(); // logs: 1
+myFunction(); // logs: 2
